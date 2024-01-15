4ba34225c3c121b943005bab0d420dc365042af2
faylun <kauan.katysuky@gmail.com>
2024-01-15 15:30:51 -0300

    Orientação a Objetos e Consulta API

--- Modulo 3/cheaperValue.js
+++ Modulo 3/cheaperValue.js
@@ -1,13 +1,15 @@
 const books = require('./bookList')
 
-let cheaper = 0
 
 function cheaperBook(arrBooks, InicialPosition){
+    let cheaper = InicialPosition
+    
     for (let current = InicialPosition; current < arrBooks.length; current++){
         if (arrBooks[current].price < arrBooks[cheaper].price){
             cheaper = current
         }
     }
+    return cheaper
 }
 
-console.log(`O livro mais barato é ${books[cheaper].price} e o titulo é ${books[cheaper].title}`)
+module.exports = cheaperBook
--- Modulo 3/selectionSort.js
+++ Modulo 3/selectionSort.js
@@ -2,5 +2,13 @@ const books = require('./bookList')
 const lowerValue = require('./cheaperValue')
 
 for (let current = 0; current < books.length; current++){
+    let lower = lowerValue(books, current)
+    
+    let currentBook = books[current]
+    let cheaperBook = books[lower]
 
+    books[current] = cheaperBook
+    books[lower] = currentBook
 }
+
+console.log(books)
