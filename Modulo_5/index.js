b4d6c098a6744a4aa211b4d55f5c7fd542e6b6cf
Faylun <kauan.katysuky@gmail.com>
2024-01-18 21:21:37 -0300

    Regex e filtrando arquivos

--- Modulo_5/index.js
+++ Modulo_5/index.js
@@ -1,6 +1,6 @@
 import chalk from 'chalk';
-import { trace } from 'console';
 import fs from 'fs';
+import { url } from 'inspector';
 
 function treatsError(erro){
     throw new Error(chalk.red(erro.code, ' | Não há arquivo para fazer a leitura.'))
@@ -13,23 +13,31 @@ function getFile(pathFile){
       .catch(treatsError)
 } */
 
-const text1 = 'A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo. São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.). [Teste de retorno 400](https://httpstat.us/404). [gatinho salsicha](http://gatinhosalsicha.com.br/)'
-
-function extractLink(text){
+async function extractLink(text){
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const capture = regex.exec(text)
-    console.log(capture)
+    const capture = [...text.matchAll(regex)]
+    const result = capture.map(cap => ({[cap[1]]: cap[2]}))
+    const objectLink = result.map((objectLink) => Object.values(objectLink).join())
+    console.log(await checkStatus(objectLink))
+}
+
+async function checkStatus(UrlList){
+    const arrStatus = Promise.all(
+        UrlList.map( async (url) => {
+            const response = await fetch(url)
+            return response.status;
+        })
+    )
+    return arrStatus
 }
 
 async function getFile(filePath){
     try{
         const text = await fs.promises.readFile(filePath, 'utf-8')
-        console.log(text)
+        extractLink(text)
     } catch (erro){
         treatsError(erro)
     }
 }
 
-//getFile('C:/Users/Kauan/Documents/PROJETOS/javascript/functions/Curso-JS/Modulo_5/texto.md');
-
-extractLink(text1)
+export default getFile
